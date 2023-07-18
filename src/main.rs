43f743f83f3846bf5ad8ad316eb53d35bfcf6270
AKORA <dragoncatm@gmail.com>
2023-07-18 12:53:35 +0200

    Init

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+const API_URL: &'static str = "https://creativecommons.tankerkoenig.de/json/list.php";
+const RADIUS: usize = 4;
+const SORT: &'static str = "price";
+const TYPE: &'static str = "diesel";
+
+fn main() {
+    println!("Hello, world!");
+}
